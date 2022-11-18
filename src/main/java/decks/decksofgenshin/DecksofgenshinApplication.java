6df9ac1fc2d832206e68e4a5b9dd10fc684fab3d
xyhuang1116 <xyhuang1116@gmail.com>
2022-11-18 15:08:11 +0100

    Decks of Genshin

---
+++ src/main/java/decks/decksofgenshin/DecksofgenshinApplication.java
@@ -0,0 +1,13 @@
+package decks.decksofgenshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecksofgenshinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecksofgenshinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/decks/decksofgenshin/DecksofgenshinApplicationTests.java
@@ -0,0 +1,13 @@
+package decks.decksofgenshin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecksofgenshinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
