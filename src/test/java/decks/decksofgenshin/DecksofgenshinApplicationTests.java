b92b7ef5011d2bbbe1590c8468b9a46ee55fc13a
xyhuang1116 <xyhuang1116@gmail.com>
2022-12-20 16:07:28 +0100

    DB connectivity test passed

--- src/test/java/decks/decksofgenshin/DecksofgenshinApplicationTests.java
+++ src/test/java/decks/decksofgenshin/DecksofgenshinApplicationTests.java
@@ -1,13 +1,45 @@
 package decks.decksofgenshin;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.*;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DecksofgenshinApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	public void givenDatasourceAvailableWhenAccessDetailsThenExpectDetails()
+	 throws SQLException {
+		assertEquals(dataSource.getClass().getName(), "com.zaxxer.hikari.HikariDataSource");
+		assertEquals(dataSource.getConnection().getMetaData().getDatabaseProductName(), "H2");
+	}
+	
+	@Test
+	public void whenCountAllCharactersThenExpectTwentySevenCourses() throws SQLException {
+		ResultSet rs = null;
+		int noOfCharacters = 0;
+
+		try(PreparedStatement ps = dataSource.getConnection().prepareStatement("SELECT COUNT(1) FROM CHARA")) {
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				noOfCharacters = rs.getInt(1);
+			}
+			assertEquals(noOfCharacters, 27L);
+		}
+		finally {
+			if(rs != null) {
+				rs.close();
+			}
+		}
 	}
 
 }
